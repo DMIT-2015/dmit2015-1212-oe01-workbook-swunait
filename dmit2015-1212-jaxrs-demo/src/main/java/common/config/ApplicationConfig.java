5a5f4632b24db1014677e3a9cbef01fc70ebb17c
Sam Wu <swu@nait.ca>
2022-02-07 20:52:39 -0700

    End of Feb 7

--- dmit2015-1212-jaxrs-demo/src/main/java/common/config/ApplicationConfig.java
+++ dmit2015-1212-jaxrs-demo/src/main/java/common/config/ApplicationConfig.java
@@ -17,8 +17,8 @@ import jakarta.enterprise.context.ApplicationScoped;
 	@DataSourceDefinition(
 		name="java:app/datasources/hsqldatabaseDS",
 		className="org.hsqldb.jdbc.JDBCDataSource",
-//		url="jdbc:hsqldb:file:~/jdk/databases/dmit2015-jaxrs-hsqldb;shutdown=true",
-		url="jdbc:hsqldb:mem:dmit2015hsqldb",
+		url="jdbc:hsqldb:file:~/jdk/databases/dmit2015-jaxrs-hsqldb3;shutdown=true",
+//		url="jdbc:hsqldb:mem:dmit2015hsqldb",
 		user="user2015",
 		password="Password2015"),
 
